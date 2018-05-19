c418ee694d1a21784e17bd733ba2a9cff2616c29
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-05-19 22:49:04 +0200

    COLORSSSSS

--- odotdot-examples/test.sh
+++ odotdot-examples/test.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 # Ghetto parallellism for the win.
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 for file in *.ö; do
     (
         cargo +stable run "$file" > /dev/null 2> /dev/null;
         if [ $? -eq 0 ]; then
-            echo "Everything is fine for $file."
+            echo "${GREEN}[✓] Everything is fine for $file.${NC}"
         else
-            echo "Stuff went wrong for $file."
+            echo "${RED}[✗] Stuff went wrong for $file.${NC}"
         fi
     ) &
 done
