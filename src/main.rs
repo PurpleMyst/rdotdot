a407fb187c17f3af6bd2d51ce45fc09c402f29f1
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-06-04 23:54:18 +0200

    Housekeeping

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+#![deny(future_incompatible)]
+#[warn(rust_2018_compatibility, rust_2018_idioms)]
 extern crate peeking_take_while;
 extern crate rpds;
 
-mod ast;
-mod parser;
+pub mod ast;
+pub mod parser;
 
 fn main() {
     let code = std::fs::read_to_string(std::env::args().nth(1).expect("USAGE: cargo run FILENAME"))
